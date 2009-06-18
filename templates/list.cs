943edb8323c48403dab5f2413c71bedea0fee896
Julien Laffaye <kimelto@gmail.com>
2009-06-18 22:10:37 +0200

    The list template now use the datas from the HDF.

--- templates/list.cs
+++ templates/list.cs
@@ -1,58 +1,45 @@
+<?cs def:print_sizes(torrent) ?>
+	<?cs var:torrent.downloaded_size ?> of <?cs var:torrent.size ?> [<?cs var:torrent.percent ?>]
+<?cs /def ?>
+
 <?cs include:"header.cs" ?>
 <div class="torrent">
-<table cellspacing="0">
-<tr>
-<td class="title">Name leech</td>
-<td rowspan="3" class="nav">
-<a href="#">Stop</a>
-<br />
-<a href="#">Delete</a>
-</td>
-</tr>
-<tr><td>
-<div class="prog-bar">
-<div class="prog-bar-leech" style="width: 33%"></div>
-</div>
-</td></tr>
-<tr><td>
-Downloading from 22 peers &ndash; 54MB of 300Mb [33%] &ndash; Down: 120kbs, Up: 20kbs
-</td></tr>
-</table>
-<table cellspacing="0">
-<tr>
-<td class="title">Name seed</td>
-<td rowspan="3" class="nav">
-<a href="#">Stop</a>
-<br />
-<a href="#">Delete</a>
-</td>
-</tr>
-<tr><td>
-<div class="prog-bar">
-<div class="prog-bar-seed" style="width: 100%"></div>
-</div>
-</td></tr>
-<tr><td>
-Downloading from 11 peers &ndash; 300Mb of 300Mb [100%] &ndash; Down: 120kbs, Up: 20kbs
-</td></tr>
-</table>
-<table cellspacing="0">
-<tr>
-<td class="title">Name in</td>
-<td rowspan="3" class="nav">
-<a href="#">Stop</a>
-<br />
-<a href="#">Delete</a>
-</td>
-</tr>
-<tr><td>
-<div class="prog-bar">
-<div class="prog-bar-inactive" style="width: 66%"></div>
-</div>
-</td></tr>
-<tr><td>
-Downloading from 11 peers &ndash; 120Mb of 300Mb [66%] &ndash; Down: 120kbs, Up: 20kbs
-</td></tr>
-</table>
+<?cs each:torrent = Torrents ?>
+	<table cellspacing="0">
+		<tr>
+			<td class="title"><?cs var:torrent.name ?></td>
+			<td rowspan="3" class="nav">
+				<?cs if:torrent.st == "inactive" ?>
+					<a href="<?cs var:CGI.ScriptName ?>?cmd=start&amp;torrent=<?cs var:torrent.num ?>">Start</a>
+				<?cs else ?>
+					<a href="<?cs var:CGI.ScriptName ?>?cmd=stop&amp;torrent=<?cs var:torrent.num ?>">Stop</a>
+				<?cs /if ?>
+				<br />
+				<a href="<?cs var:CGI.ScriptName ?>?cmd=del&amp;torrent=<?cs var:torrent.num ?>">Delete</a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<div class="prog-bar">
+					<div class="prog-bar-<?cs var:torrent.st ?>" style="width: <?cs var:torrent.percent ?>"></div>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<td>
+			<?cs if:torrent.st == "inactive" ?>
+				Inactive - <?cs call:print_sizes(torrent) ?>
+			<?cs /if ?>
+			<?cs if:torrent.st == "leech" ?>
+				Downloading from <?cs var:torrent.peers ?> peers - <?cs call:print_sizes(torrent) ?> -
+				Down: <?cs var:torrent.rate_down ?>, Up: <?cs var:torrent.rate_up ?> - Available: <?cs var:torrent.available ?>
+			<?cs /if ?>
+			<?cs if:torrent.st == "seed" ?>
+				Seeding to <?cs var:torrent.peers ?> - Up: <?cs var:torrent.rate_up ?> - Ratio: <?cs var:torrent.ratio ?>
+			<?cs /if ?>
+			</td>
+		</tr>
+	</table>
+	<?cs /each ?>
 </div>
 <?cs include:"templates/footer.cs" ?>
