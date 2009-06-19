2306f3af64e1c7a5bcee8dda5233901886458995
Julien Laffaye <kimelto@gmail.com>
2009-06-19 17:18:19 +0200

    Added support for the 'add' cmd.

--- add.c
+++ add.c
@@ -1,12 +1,69 @@
 #include "btpdcgi.h"
 
+#define GET_OR_DIE(var, name) \
+	var = hdf_get_value(cgi->hdf, name, NULL); \
+	if(var == NULL) \
+		diemsg("The '%s' config variable is needed.\n", name);
+
+int
+copy_file(FILE *src, char *path)
+{
+	FILE *dest;
+	char buf[2048];
+	size_t r,w;
+
+	dest = fopen(path, "w+");
+
+	for(;;) {
+		if((r = fread(buf, 1, sizeof buf, src)) < 1)
+			break;
+		if ((w = fwrite(buf, 1, r, dest)) != r)
+			diemsg("fwrite() : fatal error\n");
+	}
+
+	if (ferror(src))
+		diemsg("Can not read src file.\n");
+	if (ferror(dest))
+		diemsg("Can not write dest file.\n");
+
+	fclose(src);
+	fclose(dest);
+	return 0;
+}
+
 void cmd_add(CGI *cgi)
 {
-	int topdir = 0, start = 1;
+	int topdir, start;
 	size_t dirlen = 0;
-	char *dir = NULL, *name = NULL;
+	char *dir = NULL, *name = NULL, *tdir = NULL;
+	FILE *tf;
 	char *torrent = NULL;
 
+	/* Check if the form has been submited */
+	cgi_parse(cgi);
+	if(hdf_get_value(cgi->hdf, "Query.submited", NULL) == NULL) {
+		cgi_display(cgi, "add.cs");
+		return;
+	}
+
+	/* Get some params from the config file */
+	GET_OR_DIE(dir, "content_dir");
+	GET_OR_DIE(tdir, "torrents_dir");
+	dirlen = strlen(dir);
+
+	/* Get some params form the form */
+	start = hdf_get_int_value(cgi->hdf, "Query.start", 0);
+	topdir = hdf_get_int_value(cgi->hdf, "Query.topdir", 0);
+	name = hdf_get_value(cgi->hdf, "Query.name", NULL);
+
+	/* Copy the torrent file to torrents_dir */
+	tf = cgi_filehandle(cgi, "torrent_file");
+	if(tf != NULL) {
+		asprintf(&torrent, "%s/%s", tdir, hdf_get_value(cgi->hdf, "Query.torrent_file", "default"));
+		copy_file(tf, torrent);
+	}
+
+	/* We have all the infos, let's add the torrent */
 	btpd_connect();
 	char *mi;
 	size_t mi_size;
@@ -30,12 +87,14 @@ void cmd_add(CGI *cgi)
 		diemsg("make_abs_path '%s' failed (%s).\n", dpath, strerror(errno));
 	code = btpd_add(ipc, mi, mi_size, dpath, name);
 	if(code == 0 && start) {
-	struct ipc_torrent tspec;
-	tspec.by_hash = 1;
-	mi_info_hash(mi, tspec.u.hash);
-	code = btpd_start(ipc, &tspec);
+		struct ipc_torrent tspec;
+		tspec.by_hash = 1;
+		mi_info_hash(mi, tspec.u.hash);
+		code = btpd_start(ipc, &tspec);
 	}
 	if (code != IPC_OK)
 		diemsg("command failed (%s).\n", ipc_strerror(code));
+
+	cgi_display(cgi, "add.cs");
 	return;
 }
--- templates/add.cs
+++ templates/add.cs
@@ -1,10 +1,18 @@
 <?cs include:"header.cs" ?>
 <div id="add-box">
-<form id="upload" method="post" enctype="multipart/form-data" action="">
+<form id="upload" method="post" enctype="multipart/form-data" action="<?cs var:CGI.ScriptName ?>?cmd=add">
+
 <p>Select the torrent you wish to add.<br />
-<input type="file" name="torrent" size="50" /><br /><br />
-<input type="checkbox" name="start" />Automatically start the torrent.<br />
-<input type="submit" value="Add Torrent" /></p>
+<input type="file" name="torrent_file" size="50" /><br /><br />
+
+<input type="checkbox" name="start" id="start" value="1" checked="checked" />
+<label for="start">Automatically start the torrent.</label><br />
+
+<input type="checkbox" name="topdir" id="topdir" value="1" />
+<label for="topdir">Append the torrent top directory (if any) to the content path.</label><br /><br />
+
+<input type="submit" value="Add Torrent" />
+<input type="hidden" name="submited" value="1" /></p>
 </form>
 </div>
 <?cs include:"footer.cs" ?>
