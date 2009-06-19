258f9b2933364bccc51a3f9580c9996f902b14de
Julien Laffaye <kimelto@gmail.com>
2009-06-19 14:00:12 +0200

    Improved the header template
    - The <title> show the current cmd
    - The links point to the script
    - The login is correctly displayed

--- templates/header.cs
+++ templates/header.cs
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>BTPDcgi</title>
+<title>BTPDcgi - <?cs alt:Query.cmd ?>list<?cs /alt ?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href="./style.css" rel="stylesheet" type="text/css" />
 </head>
@@ -10,8 +10,11 @@
 <div id="header">
 	<div id="title">BTPDcgi</div>
 	<div id="right">
-		<div id="loggedin">Logged in as <em>kikoo</em></div>
-		<div id="menu"><a href="#">Add torrent</a> | <a href="#">List torrent</a></div>
+		<div id="loggedin">Logged in as <em><?cs alt:CGI.RemoteUser ?>anonymous<?cs /alt ?></em></div>
+		<div id="menu">
+			<a href="<?cs var:CGI.ScriptName ?>?cmd=add">Add torrent</a> |
+			<a href="<?cs var:CGI.ScriptName ?>">List torrent</a>
+		</div>
 	</div>
 </div>
 
